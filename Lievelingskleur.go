63bdb86c128fe2c4d4ad2c7070ac8fd3b2546723
FreekStelten <124879964+FreekStelten@users.noreply.github.com>
2023-03-09 11:03:46 +0100

    Lievelingskleur werkend Via Switch.

--- Lievelingskleur.go
+++ Lievelingskleur.go
@@ -10,15 +10,20 @@ func main() {
     fmt.Print("voer een kleur in: ")
     fmt.Scanln(&kleur)
 
-    if kleur == "rood" {fmt.Println("rood met passie")  }
-
-    if kleur == "blauw" {fmt.Println("zo blauw als de lucht") }
-
-	if kleur == "geel" {fmt.Println("geel als de stralen van de zon") }
-
-	if kleur == "groen" {fmt.Println("groen als de natuur")}
-
-	else {fmt.Println("deze kleur is niet geregistreerd.")}
-    
-	fmt.Printf("Je hebt de kleur %s ingevoerd.\n", kleur)
+    switch kleur {
+    case "rood":
+        fmt.Println("rood met passie")
+    case "blauw":
+        fmt.Println("zo blauw als de lucht")
+    case "geel":
+        fmt.Println("geel als de stralen van de zon")
+    case "groen":
+        fmt.Println("groen als de natuur")
+	case "paars":
+		fmt.Println("Paarse vrijdag")
+	case "oranje": 
+		fmt.Println("kleur van Nederland.")
+    default:
+        fmt.Println("deze kleur is niet geregistreerd.")
+    }
 }
