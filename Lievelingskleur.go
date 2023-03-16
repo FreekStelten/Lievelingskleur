aef609b2efe04b1469ab52f6ca81edfe9aa303fb
FreekStelten <124879964+FreekStelten@users.noreply.github.com>
2023-03-16 12:58:19 +0100

    Robuustheid waargebrorgd door Bool gebruiken

--- Lievelingskleur.go
+++ Lievelingskleur.go
@@ -11,55 +11,74 @@ type Colors struct {
         Name string `json:"name"`
     } `json:"colors"`
 }
-func Filenotfound (){
+// als er een fout optreed bij het lezen van bestand dan wordt er error teruggegeven en wordt bool false
+//als bestand succesvol gelezen wordt dan komt die op bool op true en nil te staan
+func readJSONFile(filePath string) (bool, error) { 
+    file, err := ioutil.ReadFile(filePath)
+    if err != nil {
+        return false, err
+    }
 
- // Lees het JSON-bestand
- func (getcolor)
- file, err := ioutil.ReadFile("colors.json")
- if err != nil {
-     fmt.Println("Error reading JSON file:", err)
-     return func Filenotfound
- }
-
-}
-func main() {
-   
-    
-    // Decodeer de JSON-data, haalt de data eruit.
     var colors Colors
     err = json.Unmarshal(file, &colors)
     if err != nil {
-        fmt.Println("Error decoding JSON file:", err)
-        return
+        return false, err
     }
 
-    // Toont de kleurnamen in de terminal
-    fmt.Println("kies een van de onderstaande kleuren(nummer):")
-    for i, color := range colors.Colors {
-        fmt.Printf("%d. %s\n", i+1, color.Name)
+    return true, nil
+}
+//bij de main wordt er gekeken of de bool true of false staat. 
+func main() {
+    success, err := readJSONFile("colors.json")
+    if err != nil {
+        fmt.Println("Error reading JSON file:", err)
     }
 
-    // Kies een kleur met een switch-statement
-    var choice int
-    fmt.Scanln(&choice)
+    if success {
+        var colors Colors
 
-    var chosenColor string
-    switch choice {
-    case 1:
-        chosenColor = colors.Colors[0].Name
-    case 2:
-        chosenColor = colors.Colors[1].Name
-    case 3:
-        chosenColor = colors.Colors[2].Name
-    case 4:
-        chosenColor = colors.Colors[3].Name
-    case 5:
-        chosenColor = colors.Colors[4].Name   
-    default:
-        fmt.Println("foute keuze")
-        return 
+        // Lees het JSON-bestand
+        file, err := ioutil.ReadFile("colors.json")
+        if err != nil {
+            fmt.Println("Error reading JSON file:", err)
+            return
+        }
+
+        // Decodeer de JSON-data, haalt de data eruit.
+        err = json.Unmarshal(file, &colors)
+        if err != nil {
+            fmt.Println("Error decoding JSON file:", err)
+            return
+        }
+
+        // Toont de kleurnamen in de terminal
+        fmt.Println("kies een van de onderstaande kleuren(nummer):")
+        for i, color := range colors.Colors {
+            fmt.Printf("%d. %s\n", i+1, color.Name)
+        }
+
+        // Kies een kleur met een switch-statement
+        var choice int
+        fmt.Scanln(&choice)
+
+        var chosenColor string
+        switch choice {
+        case 1:
+            chosenColor = colors.Colors[0].Name
+        case 2:
+            chosenColor = colors.Colors[1].Name
+        case 3:
+            chosenColor = colors.Colors[2].Name
+        case 4:
+            chosenColor = colors.Colors[3].Name
+        case 5:
+            chosenColor = colors.Colors[4].Name   
+        default:
+            fmt.Println("foute keuze")
+            return
+        }
+
+        // Toon de gekozen kleur in de terminal
+        fmt.Printf("je kiest kleur %s\n", chosenColor)
     }
-
-    // Toon de gekozen kleur in de terminal
-    fmt.Printf("je kiest kleur %s\n", chosenColor)
 }
